600ed2d76d6afff8cdb02315ad4cccd6d51db4a0
hyeondobin <dobinhyeon@gmail.com>
2025-12-08 12:50:54 +0900

    enable bluetooth
    add bluemn-applet to control bluetooth

--- home/default.nix
+++ home/default.nix
@@ -1,14 +1,17 @@
-{pkgs, config, ... }:{
-    home.stateVersion = "25.05";
-    xdg.configFile."uwsm/env".source = "${config.home.sessionVariablesPackage}/etc/profile.d/hm-session-vars.sh"; 
-    imports = [
-        ./hyprland
-        ./cli.nix
-        ./waybar
-        ./git.nix
-        ./vimb.nix
-        ./messanger.nix
-        ./nvim.nix
-        ./catppuccin.nix
-    ];
+{ pkgs, config, ... }:
+{
+  home.stateVersion = "25.05";
+  xdg.configFile."uwsm/env".source =
+    "${config.home.sessionVariablesPackage}/etc/profile.d/hm-session-vars.sh";
+  imports = [
+    ./hyprland
+    ./cli.nix
+    ./waybar
+    ./git.nix
+    ./vimb.nix
+    ./messanger.nix
+    ./nvim.nix
+    ./catppuccin.nix
+    ./bluetooth.nix
+  ];
 }
--- modules/default.nix
+++ modules/default.nix
@@ -1,16 +1,17 @@
 { pkgs, ... }:
 {
-    fonts.packages = with pkgs; [
-        nerd-fonts.jetbrains-mono
-    ];
-    imports = [
-        ./nix.nix
-        ./qmk.nix
-        # ./emacs.nix
-    ];
-    # hardware.bluetooth.enable = true;
-    # services.blueman.enable = true;
-    environment.systemPackages = with pkgs; [
-        wl-clipboard
-    ];
+  fonts.packages = with pkgs; [
+    nerd-fonts.jetbrains-mono
+    nerd-fonts.d2coding
+  ];
+  imports = [
+    ./nix.nix
+    ./qmk.nix
+    # ./emacs.nix
+  ];
+  hardware.bluetooth.enable = true;
+  services.blueman.enable = true;
+  environment.systemPackages = with pkgs; [
+    wl-clipboard
+  ];
 }
